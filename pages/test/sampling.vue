0dc43053587e7b1f2a906d93a9dd592b10ad4204
swc9803 <swc9803@gmail.com>
2023-11-22 22:23:57 +0900

    sampling setting

--- pages/test/sampling.vue
+++ pages/test/sampling.vue
@@ -1,7 +1,75 @@
 <template>
-  <div></div>
+  <div>
+    <div ref="canvasRef" class="canvas" />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-<style lang="scss" scoped></style>
+const canvasRef = ref();
+let camera;
+let renderer;
+let raf;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: 0x66ccff,
+  wireframe: true,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const animate = () => {
+  renderer.render(scene, camera);
+
+  raf = requestAnimationFrame(animate);
+};
+
+const onResize = () => {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(canvasRef.value.offsetWidth, canvasRef.value.offsetHeight);
+  canvasRef.value.appendChild(renderer.domElement);
+  camera.aspect = canvasRef.value.offsetWidth / canvasRef.value.offsetHeight;
+  camera.updateProjectionMatrix();
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+};
+
+onMounted(() => {
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    canvasRef.value.offsetWidth / canvasRef.value.offsetHeight,
+    0.1,
+    1000,
+  );
+  camera.position.set(1, 1, 2);
+
+  onResize();
+  animate();
+
+  window.addEventListener("resize", onResize);
+});
+
+onBeforeUnmount(() => {
+  cancelAnimationFrame(raf);
+
+  window.removeEventListener("resize", onResize);
+});
+</script>
+
+<style lang="scss" scoped>
+.canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
+  transform: translate3d(0, 0, 0);
+  width: 100%;
+  height: 100vh;
+}
+</style>
